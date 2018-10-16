65f7952dd06ff5bcd8ac0aef58b9f578b6fa7e94
Jason <github@jallan.co.uk>
2018-10-16 17:42:06 +0100

    Updating electron and electron supporting code

--- server.js
+++ server.js
@@ -13,43 +13,36 @@ const config = require('./webpack.config.development')
 
 const argv = require('minimist')(process.argv.slice(2))
 
-const app = express()
 const compiler = webpack(config)
 const PORT = process.env.PORT || 3000
-
 const wdm = webpackDevMiddleware(compiler, {
 	publicPath: config.output.publicPath,
-	stats: {
-		colors: true,
-	},
+	stats: { colors: true },
 })
 
-app.use(wdm)
+const server = express()
+	.use(wdm)
+	.use(webpackHotMiddleware(compiler))
+	.listen(PORT, 'localhost', serverError => {
+		if (serverError) {
+			return console.error(serverError)
+		}
 
-app.use(webpackHotMiddleware(compiler))
+		if (argv['start-hot']) {
+			spawn('npm', ['run', 'start-hot'], {
+				shell: true,
+				env: process.env,
+				stdio: 'inherit',
+			})
+				.on('close', (code = 0) => process.exit(code))
+				.on('error', spawnError => console.error(spawnError))
+		}
 
-const server = app.listen(PORT, 'localhost', serverError => {
-	if (serverError) {
-		return console.error(serverError)
-	}
-
-	if (argv['start-hot']) {
-		spawn('npm', ['run', 'start-hot'], {
-			shell: true,
-			env: process.env,
-			stdio: 'inherit',
-		})
-			.on('close', code => process.exit(code))
-			.on('error', spawnError => console.error(spawnError))
-	}
-
-	console.log(`Listening at http://localhost:${PORT}`)
-})
+		console.log(`Listening at http://localhost:${PORT}`)
+	})
 
 process.on('SIGTERM', () => {
 	console.log('Stopping dev server')
 	wdm.close()
-	server.close(() => {
-		process.exit(0)
-	})
+	server.close(() => process.exit(0))
 })
