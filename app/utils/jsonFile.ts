239ebe8d3107eafda65b5ed9fe53c125210155b7
Jason <github@jallan.co.uk>
2018-10-17 18:52:32 +0100

    writeJSON graciously respects pre-existing indents

--- app/utils/jsonFile.ts
+++ app/utils/jsonFile.ts
@@ -1,4 +1,12 @@
-import { writeFile, readFile } from './fsPath'
+import { writeFile, readFile, exists } from './fsPath'
+
+const detectIndent: (
+	json: string
+) => {
+	amount: number
+	type: null | 'space' | 'tab'
+	indent: string
+} = require('detect-indent')
 
 export async function readJSON<T = {}>(path: string): Promise<T | void> {
 	try {
@@ -9,6 +17,21 @@ export async function readJSON<T = {}>(path: string): Promise<T | void> {
 	}
 }
 
-export function writeJSON(path: string, data: any) {
-	return writeFile(path, JSON.stringify(data, undefined, '/t'))
+export async function writeJSON(path: string, data: any, indent = '\t') {
+	if (await exists(path)) {
+		const contents = await readFile(path)
+
+		if (contents) {
+			const text = contents.toString()
+
+			try {
+				// Check if text parses as JSON
+				JSON.parse(text)
+
+				indent = detectIndent(text).indent
+			} catch (e) {}
+		}
+	}
+
+	return writeFile(path, JSON.stringify(data, undefined, indent) + '\n')
 }
