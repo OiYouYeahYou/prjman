ec0a1dbec2d529a160b8723215f931ffffbb2f4f
Jason <github@jallan.co.uk>
2018-10-17 17:37:34 +0100

    Removing dead code

--- app/structures/Settings.ts
+++ app/structures/Settings.ts
@@ -11,11 +11,3 @@ export class Settings {
 		options.paths.map(path => this.collections.add(path, true))
 	}
 }
-
-function validatePaths(paths: string[]) {
-	return paths.some(path => validatePath(path))
-}
-
-function validatePath(path: string) {
-	return typeof path === 'string'
-}
