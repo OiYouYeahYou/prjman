0ecb7f3203414c11cd753a17190388a3b950a61a
Jason <github@jallan.co.uk>
2018-10-17 18:13:12 +0100

    Setting HomePage to update on ProjectStore change

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -4,13 +4,19 @@ import { projectStore } from '../projects'
 import RemoteGetter from '../components/RemoteGetter'
 
 export class HomePage extends React.Component<{}, {}> {
+	readonly updateFn = () => this.forceUpdate()
+
+	componentWillUnmount() {
+		projectStore.removeListener('change', this.updateFn)
+	}
+
 	render() {
+		projectStore.on('change', this.updateFn)
+
 		const entries = projectStore.entries()
 		let list
 
 		if (!projectStore.isReady) {
-			projectStore.once('ready', () => this.forceUpdate())
-
 			list = <b>loading project data</b>
 		} else if (entries.length) {
 			const items = entries.map(([name]) => (
